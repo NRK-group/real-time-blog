da2a343da8786aa6b2f1d0fc0b883e2cae3a097a
RickyAdriell <53133228+codecplyre@users.noreply.github.com>
2022-08-11 12:13:25 +0100

    create a golang server

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("frontend/index.html")
+	if err != nil {
+		http.Error(w, "500 Internal error", http.StatusInternalServerError)
+		return
+	}
+	if err := t.Execute(w, ""); err != nil {
+		http.Error(w, "500 Internal error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	frontend := http.FileServer(http.Dir("./frontend"))
+	http.Handle("/frontend/", http.StripPrefix("/frontend/", frontend)) // handling the CSS
+	fmt.Printf("Starting server at port 8800\n")
+	log.Fatal(http.ListenAndServe(":8800", nil))
+}
