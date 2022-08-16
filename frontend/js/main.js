fff43782071862636ba6a46fc1dd07526e664e05
keivon <keivon.991@gmail.com>
2022-08-16 11:57:30 +0100

    added login query

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -78,6 +78,7 @@ const getloginData = () => {
 const loginBtn = document.querySelector('#login-btn-id');
 loginBtn.addEventListener('click', (e) => {
     e.preventDefault();
+    console.log("that")
     const userLoginData = getloginData();
     fetch('/login', {
         method: 'POST',
